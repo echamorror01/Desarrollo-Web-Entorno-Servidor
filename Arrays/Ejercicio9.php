43c9e4ca071514aec92babfa4af9e52277c75e53
echamorror01 <echamorror01@saurezdefigueroa.es>
2025-10-30 19:13:56 +0100

    ok

---
+++ Arrays/Ejercicio9.php
@@ -0,0 +1,11 @@
+<?php
+/*Crea un mini-diccionario español-inglés que contenga, al menos, 20 palabras (con su traducción). Utiliza un array asociativo para almacenar las parejas de palabras. 
+El programa pedirá una palabra en español y 
+dará la correspondiente traducción en inglés.*/
+
+
+
+
+
+
+?>
