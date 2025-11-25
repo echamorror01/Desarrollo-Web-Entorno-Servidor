a99584c9ef65bed559526e331ccff00d83873fd0
echamorror01 <echamorror01@saurezdefigueroa.es>
2025-11-25 21:35:39 +0100

    ok

---
+++ Ficheros/Ejercicio12/Ejercicio12.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <div class="color">
+    <form action="" method="get">
+    <h2>Introducir contenido</h2>
+   <label for="contenido">Introducir contenido</label><br>
+   <input type="text" name="contenido" id="contenido" placeholder="Teclee el contenido"  ><br>
+   <label for="contnual">Desea continuar¿(S/N)?</label><br>
+    <input type="submit" name="accion" value="Si">
+    <input type="submit" name="accion" value="No">
+    </form>
+     </div>
+
+
+
+<?php
+//Capturar los datos  
+$contenido=isset($_GET["contenido"]) ? $_GET["contenido"] : " ";
+$accion=isset($_GET["accion"]) ? $_GET["accion"] : " ";
+//Si presiona si 
+if(!empty($contenido) && $accion=="Si"){
+ //si no está vacio 
+ $archivo=fopen("datosEjercicio.txt","a"); //la a para agregar contenido
+     if($archivo) {
+    //Escribir el arhivo 
+    fwrite($archivo,$contenido . PHP_EOL); // ES UN SALTO DE LINEA 
+    //Cerramos el archivo
+    fclose($archivo);
+    //Mensaje de éxito 
+    echo"Grabado";
+     }else{
+        echo"Error al abrir o al crear el archivo";
+    }
+}
+//si marca que no 
+if($accion=="No"){
+    $archivo=fopen("datosEjercicio.txt","r");
+    if($archivo){
+        while(($linea=fgets($archivo)!==false)){
+            echo $linea . "<br>";
+        }
+        fclose($archivo);
+    }else{
+        echo "Error";
+    }
+}
+
+ 
+
+?>
+</body>
+</html>
